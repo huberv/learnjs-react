8051500a38a7bb8f4a710dcf2e25c74a38e8b1a4
Valentin Huber <Valentin.Huber@msg-systems.com>
2017-07-11 08:48:15 +0200

    Landing page (p. 62)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hero from './images/HeroImage.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="App">
+          <div className="row">
+            <div className="one-half column">
+              <h3>Learn JavaScript, one puzzle at a time.</h3>
+              <a href="" className="button button-primary">Start Now!</a>
+            </div>
+            <div className="one-half column">
+              <img src={Hero} alt="Cube"></img>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
